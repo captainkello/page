b7f89afb73b28e01bd8c7e6f9fc5c5a1170e1113
Capt. Kello <captainkello@gmail.com>
2010-11-19 11:21:57 -1000

    fixed micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101119195502) do
+ActiveRecord::Schema.define(:version => 20101119204223) do
 
   create_table "members", :force => true do |t|
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "member_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
